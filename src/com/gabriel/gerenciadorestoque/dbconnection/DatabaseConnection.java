74926a06fa6f4822cadbebfd4e2b149719a6cba6
Gabriel Paredes <116836040+ggabrielparedes@users.noreply.github.com>
2025-04-22 19:25:24 -0300

    Update DatabaseConnection.java

--- src/com/gabriel/gerenciadorestoque/dbconnection/DatabaseConnection.java
+++ src/com/gabriel/gerenciadorestoque/dbconnection/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL  = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USERNAME = "C##GABRIEL"; //Username
-    private static final String PASSWORD = "123"; //Password
+    private static final String USERNAME = ""; //Username
+    private static final String PASSWORD = ""; //Password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
